84336d1b44f125e217c0289fd7c75e447a6395cd
Prakash4844 <pk484442@gmail.com>
2023-06-09 18:38:58 +0530

    Now Pages can be generated Using Page count.
    1. Added Locale Selector from locale_list
    2. Integrated Terminal Windows
    3. Added Generate and Exit buttons
    
    TODO1: Check if Page List Textbox can be used if not implement it.
    TODO2: Add option to Add _index.locale_code.md Option with it's own Archtype.
    TODO3: Might need to Adjust UI and Add icons if possible

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from tkinter import filedialog
 import customtkinter as ctk
-import subprocess
+from tkterminal import Terminal
 
 app = ctk.CTk()
 app.title("Hugo Page Generator")
@@ -24,6 +24,7 @@ def select_path():
     global Folder_PATH
     Folder_PATH = filedialog.askdirectory()
     os.chdir(Folder_PATH)
+    Terminal.run_command("cd " + Folder_PATH)
     Folder_textbox.insert(index=0.0, text=Folder_PATH)
     update_archtype_list()
 
@@ -42,7 +43,7 @@ Archtype_label = ctk.CTkLabel(app, text="Select Archtype")
 Archtype_label.place(relx=0.01, rely=0.17)
 
 
-def option_menu_callback(choice):
+def archtype_menu_callback(choice):
     """
     Set Archtype in ArchType_menu
 
@@ -78,7 +79,7 @@ def update_archtype_list():
     ArchType_menu.configure(values=current_list)
 
 
-ArchType_menu = ctk.CTkOptionMenu(app, values=get_archtypes(), command=option_menu_callback)
+ArchType_menu = ctk.CTkOptionMenu(app, values=get_archtypes(), command=archtype_menu_callback)
 ArchType_menu.place(relx=0.01, rely=0.23)
 ArchType_menu.set("default")
 
@@ -86,7 +87,7 @@ ArchType_menu.set("default")
 
 # ------------------------ Custom PATH STARTS -----------------------------------------------------
 
-Custom_Path_label = ctk.CTkLabel(app, text="Put custom path here(optional) Ex: /blog/")
+Custom_Path_label = ctk.CTkLabel(app, text="Put custom path here(optional) Ex: content/blog/")
 Custom_Path_label.place(relx=0.01, rely=0.3)
 Custom_Path_textbox = ctk.CTkTextbox(app, width=250, height=30)
 Custom_Path_textbox.place(relx=0.01, rely=0.36)
@@ -125,5 +126,80 @@ Page_list_textbox.place(relx=0.01, rely=0.67)
 
 # ------------------------ Page List Ends --------------------------------------------------------
 
+# ------------------------ Locale Selector Starts -------------------------------------------------
 
+
+def locale_menu_callback(choice):
+    """
+    Set Locale in Locale_menu
+    :param choice:
+    :return: none
+    """
+    Locale_menu.set(choice)
+
+
+Locale_label = ctk.CTkLabel(app, text="Select Locale")
+Locale_label.place(relx=0.45, rely=0.2)
+with open("locale_list", "r") as file:
+    locale_list = file.readlines()
+    locale_list = [locale.strip() for locale in locale_list]
+
+Locale_menu = ctk.CTkOptionMenu(app, values=locale_list, dynamic_resizing=False, command=locale_menu_callback)
+Locale_menu.place(relx=0.45, rely=0.26)
+
+# ------------------------ Output terminal Starts -------------------------------------------------
+
+Terminal_label = ctk.CTkLabel(app, text="Output Terminal")
+Terminal_label.place(relx=0.45, rely=0.4)
+Terminal = Terminal(app, relief="sunken", background="#272629", foreground="white", font="Hack 8", height=15, width=60,
+                    cursor="arrow", takefocus=False)
+Terminal.basename = "Zaphkiel $"
+Terminal.place(relx=0.45, rely=0.45)
+Terminal.shell = True
+
+
+# ------------------------ Output terminal Ends ---------------------------------------------------
+
+# ------------------------ Generate Button Starts -------------------------------------------------
+
+def generate():
+    """
+    Generate pages in Hugo Project
+    :return:
+    """
+    Boilerplate_command = 'hugo new --kind'
+    Archtype = ArchType_menu.get()
+
+    Custom_Path = Custom_Path_textbox.get("0.0", "end")
+    Custom_Path = Custom_Path.replace("\n", "")
+    if Custom_Path == "":
+        Custom_Path = "/"
+
+    locale = Locale_menu.get()
+
+    Page_list = Page_list_textbox.get("0.0", "end")
+    Page_list = Page_list.split("\n")
+    if Page_list == ['', '']:
+        page_count_slider_value = page_count_slider.get()
+        Page_list = [(Archtype + str(i)).strip() + f'.{locale}.md' for i in range(1, int(page_count_slider_value) + 1)]
+    for page in Page_list:
+        Custom_page = f'''"{Custom_Path}{page}"'''
+        command = Boilerplate_command + " " + Archtype + " " + Custom_page
+        command.strip()
+        Terminal.run_command(command)
+        Terminal.clear()
+    Terminal.clear()
+
+
+Generate_button = ctk.CTkButton(app, text="Generate", font=("Noto Sans", 20), fg_color="#59C837", hover_color="#476A4A",
+                                text_color="#000000", command=generate)
+Generate_button.place(relx=0.45, rely=0.9)
+
+Exit_button = ctk.CTkButton(app, text="Exit", command=app.destroy, font=("Noto Sans", 20), fg_color="#E9524A",
+                            hover_color="#854040")
+Exit_button.place(relx=0.75, rely=0.9)
+
+# ------------------------ Generate Button Ends ---------------------------------------------------
+
+# ------------------------ Main Loop ---------------------------------------------------------------
 app.mainloop()
